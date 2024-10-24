b3cbbc75f87e51d89c3b6de38c378003d6c39645
maheshyadavsrn <maheshyadavsrn2022@gmail.com>
2024-10-24 11:00:50 +0530

    first commit

---
+++ src/main/java/com/project/ProjectManagementBakendApplication.java
@@ -0,0 +1,14 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementBakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementBakendApplication.class, args);
+		System.out.println("Server Running Successfully!!");
+	}
+
+}
